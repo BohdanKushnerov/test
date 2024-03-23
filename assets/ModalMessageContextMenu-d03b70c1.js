13249e7895e39a344fb149c1a45e296017fe472b
BohdanKushnerov <bohdan.kushnerov@gmail.com>
2024-03-23 18:46:45 +0000

    Deploying to gh-pages from @ BohdanKushnerov/test@ecdc6d070e6c1d5efad014c8f8c42446ef38afae 🚀

--- assets/ModalMessageContextMenu-d03b70c1.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-b48e6dac.js";import{u as n}from"./useCloseModal-266f730d.js";const p=({closeModal:t,modalPosition:e,children:s})=>(n(t),o.jsx("div",{style:{position:"absolute",top:e.top+"px",left:e.left+"px"},className:"z-50 w-screen h-screen bg-transparent pointer-events-none",children:s}));export{p as default};
